4e64446142d51d25a67be886017d930d8aa71767
PoojaSubramanyaRaghu <subramanyaraghu.p@northeastern.edu>
2026-06-25 22:31:54 -0400

    Day 1: environment setup complete

---
+++ dbt/healthcare_dq/logs/query_log.sql
@@ -0,0 +1,9 @@
+-- created_at: 2026-06-26T02:18:46.973407+00:00
+-- finished_at: 2026-06-26T02:18:47.081587+00:00
+-- elapsed: 108ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: not available
+-- query_id: 01c54c4a-3204-26d6-0008-056a000620ee
+-- desc: not provided
+select 1 as id;
